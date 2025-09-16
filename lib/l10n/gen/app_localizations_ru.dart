bc3ac3ce35c47d3d79c08a4d00ea70fb68d231af
Vladislav Danshov <vladislavdanshov01@gmail.com>
2025-09-16 22:22:08 +0300

    setup localizations & analysis & basic packages

---
+++ lib/l10n/gen/app_localizations_ru.dart
@@ -0,0 +1,13 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class StringsRu extends Strings {
+  StringsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get helloWorld => 'Привет, Мир!';
+}
