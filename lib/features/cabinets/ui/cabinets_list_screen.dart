0f8e05ac789da576a6716a60b4bc0fc9e1507907
waka-musha <danila.lobov2409@mail.ru>
2025-11-05 12:15:54 +0300

    fix(cabinet, screen): remove unused import

--- lib/features/cabinets/ui/cabinets_list_screen.dart
+++ lib/features/cabinets/ui/cabinets_list_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../app/navigation/navigation_ext.dart';
 import '../../../common/ui/profile_app_bar_action.dart';
